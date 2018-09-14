1dd20c215b0581917adcab5b41f59bf8bceddf0b
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2018-09-14 21:48:15 +0200

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let x = None
+
+[<EntryPoint>]
+let main argv =
+    x.Value
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
